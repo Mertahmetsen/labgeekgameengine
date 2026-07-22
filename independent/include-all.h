8d7270d5be4b8e1e5607dd7d44fccfb72b56c8dc
Mert Ahmet Şen <mertahmetsen@gmail.com>
2026-07-22 12:07:23 +0300

    Minor convinience update
    TECHNICAL STUFF: You can now include every header with
    "independent/include-all.h".

---
+++ independent/include-all.h
@@ -0,0 +1,14 @@
+#ifndef MERAHM_ALL_INCLUDE
+#define MERAHM_ALL_INCLUDE
+
+#include "../fundamental/basic.h"
+#include "../fundamental/globals.h"
+#include "../render/cam-zoomscaling.h"
+#include "../render/mouse-cam-control.h"
+#include "../render/render-groups.h"
+#include "../render/texture.h"
+#include "../ui/cursor.h"
+#include "../ui/textbox-raygui.h"
+#include "../ui/window-pos.h"
+
+#endif
