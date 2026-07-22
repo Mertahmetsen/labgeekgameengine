66237ab4cd2aa498e9d1a8a265c5991296b5056f
Mert Ahmet Şen <mertahmetsen@gmail.com>
2026-07-22 13:24:45 +0300

    Added more window position helper functions
    TECHNICAL STUFF: getWindowPosFromRect() and slideWindowPosFromRect()
    are basically the same as their counterparts, but they can take
    rectangles as references.

--- ui/window-pos.h
+++ ui/window-pos.h
@@ -19,10 +19,34 @@ IntVector getWindowPos (WindowPos pos) {
     }
 }
 
+// the reference becomes the rectangle rather than the entire window
+IntVector getWindowPosFromRect (Rectangle rect, WindowPos refpos) {
+    switch (refpos) {
+        case TOPLEFT:
+            return ivec(rect.x, rect.y);
+        case TOPRIGHT:
+            return ivec(rect.x + rect.width, rect.y);
+        case BOTLEFT:
+            return ivec(rect.x, rect.y + rect.height);
+        case BOTRIGHT:
+            return ivec(rect.x + rect.width, rect.y + rect.height);
+        case MID:
+            return ivec(rect.x + (rect.width/2), rect.y + (rect.height/2));
+        default:
+            return ivec(0,0);
+    }
+}
+
 void slideWindowPos (IntVector* pos, IntVector slide, float ratio) {
     if (!pos) return;
     pos->x += (int)floorf((float)(slide.x - pos->x) * ratio);
     pos->y += (int)floorf((float)(slide.y - pos->y) * ratio);
+}
+
+IntVector slideWindowPosFromRect (Rectangle rect, WindowPos refpos, IntVector slide, float ratio) {
+    IntVector wpfr = getWindowPosFromRect(rect, refpos);
+    slideWindowPos(&wpfr, slide, ratio);
+    return wpfr;
 }
 
 #endif
