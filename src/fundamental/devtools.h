929abaa928c87fadb9146a16c07512a8a5b9fa74
Mert Ahmet Şen <mertahmetsen@gmail.com>
2026-08-19 15:10:37 +0300

    Improved recompilation function

--- src/fundamental/devtools.h
+++ src/fundamental/devtools.h
@@ -2,6 +2,31 @@
 #define MERAHM_DEVTOOLS
 #include "basic.h"
 
+int recompileBinary (int argc, char** argv) {
+    if (g_b_alreadyRecompiled) return 0;
+    int result = system(TextFormat("gcc %s -o %s %s", g_b_mainSourcePath, g_b_binaryName, g_b_gccFlags));
+    if (result != 0) {
+        TraceLogCaller(LERR, "Could not recompile: GCC exited with code %d", result);
+        return result;
+    }
+    char** newArgv = malloc ((argc+2)*sizeof(*newArgv));
+    if (!newArgv) {
+        logBasic(E_NOMEM);
+        return -1;
+    }
+    newArgv[0] = argv[0];
+    newArgv[1] = "--skip-recompilation";
+    for (int i=1; i<argc; ++i) {
+        newArgv[i+1] = argv[i];
+    }
+    newArgv[argc+1] = NULL;
+
+    execv("./bin", newArgv);
+    TraceLogCaller(LERR, "Internal error: execv failed.");
+    free(newArgv);
+    return -1;
+}
+
 void argTools (int argc, char** argv) {
     if (argc < 2) return;
     for (int i=1; i<argc; ++i) {
@@ -16,29 +41,10 @@ void argTools (int argc, char** argv) {
             g_b_verboseStatus = (VerboseStatus)TextToInteger(argv[++i]);
         }
         else if (TextIsEqual(argv[i], "--recompile")) {
-            if (g_b_alreadyRecompiled) continue;
-            int result = system(TextFormat("gcc %s -o %s %s", g_b_mainSourcePath, g_b_binaryName, g_b_gccFlags));
-            if (result != 0) {
-                TraceLogCaller(LERR, "Could not recompile: GCC exited with code %d", result);
-                return;
+            const int rbin = recompileBinary(argc, argv);
+            if (rbin != 0) {
+                TraceLogCaller(LERR, "Compilation failed: Compilation function returned %d", rbin);
             }
-
-            char** newArgv = malloc ((argc+2)*sizeof(*newArgv));
-            if (!newArgv) {
-                logBasic(E_NOMEM);
-                return;
-            }
-            newArgv[0] = argv[0];
-            newArgv[1] = "--skip-recompilation";
-            for (int i=1; i<argc; ++i) {
-                newArgv[i+1] = argv[i];
-            }
-            newArgv[argc+1] = NULL;
-
-            execv("./bin", newArgv);
-            TraceLogCaller(LERR, "Internal error: execv failed.");
-            free(newArgv);
-            return;
         }
     }
 }
